344bffa375cf62b45f405386560aa00888578336
diego <diegsagdemig@hotmail.com>
2019-10-02 11:56:20 +0200

    Añadido estado tienda. Cambios menores en estados level, settings, menu, creditos y endgame

--- docs/src/states/credits.js
+++ docs/src/states/credits.js
@@ -16,7 +16,12 @@ PunchemOut.creditsState.prototype = {
     },
 
     create: function () {
+        nombres = game.add.text(game.world.centerX, 300, 
+            "Alejandro Hernández Pérez\nMario Márquez Balduque\nGabriel Muñoz Borchers\nCarlos Ventura Padina González\nDiego Sagredo de Miguel", style);
+        nombres.anchor.setTo(0.5)
 
+        back = this.add.button(50, 500, 'skeleton', function() { game.state.start('menuState'); }, this, 2, 1, 0);
+        backText = game.add.text(110, 520, "Volver", style);
     },
 
     update: function () {
